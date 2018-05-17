00a5134869944e80639dbf998c838151182660e5
mark.ma <mark.ma@blockshine.com>
2018-05-17 21:00:39 +0800

    udpate conf.ini

--- com/morningtech/bts/adapter/wallet.py
+++ com/morningtech/bts/adapter/wallet.py
@@ -6,11 +6,14 @@ bitshares = BitShares()
 is_created = bitshares.wallet.created()
 
 conf = configparser.ConfigParser()
-conf.read('/Users/maxiaodong/privateChain/bts-service/conf.ini', encoding="utf8")
+conf.read('/config/bitshares/conf.ini', encoding="utf8")
 
 password = conf.get('wallet', 'wallet_password')
-online_private_key = conf.get('wallet', 'on_line_wallet_private_key')
-print("online_private_key:", online_private_key)
+
+# only for account operation, should be moved into function in the future.  mark.ma
+online_account_private_key = conf.get('wallet', 'on_line_account_private_key')
+
+print("online_account_private_key", online_account_private_key)
 
 if not is_created:
     bitshares.wallet.create(password)
@@ -18,11 +21,14 @@ if not is_created:
 
 def transfer(to, amount, asset, memo, fromAccount):
     print("password:", password)
+    # only for transfer use.on_line_account_private_key
+    online_fund_private_key = conf.get('wallet', 'on_line_wallet_private_key')
+    print("online_fund_private_key:", online_fund_private_key)
 
     bitshares.wallet.unlock(password)
     # need to read addPrivateKey from configuration shell.
     try:
-        bitshares.wallet.addPrivateKey(online_private_key)
+        bitshares.wallet.addPrivateKey(online_fund_private_key)
     except Exception:
         pass
 
